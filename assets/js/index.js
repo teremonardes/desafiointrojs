f65e86b77d2bd90f499f713821f13c95103a0da6
Tere Monardes <teremonardes.s@gmail.com>
2024-09-08 11:56:52 -0300

    funcionando

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,9 @@
-precio = 400000
+precio = 400000;
 
 precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+precioSpan.innerHTML = precio;
 // cantidadSpan= document.querySelector(".cantidad")
 // cantidadSpan.innerHTML = cantidad
+
+// let nuevoPrecio = precio * cantidad;
+// precioSpan.innerHTML = nuevoPrecio;
