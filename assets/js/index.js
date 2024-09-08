9cbbeee844962a6328db17f907774642d7ff737f
Tere Monardes <teremonardes.s@gmail.com>
2024-09-08 10:15:46 -0300

    base

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+precio = 400000
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+// cantidadSpan= document.querySelector(".cantidad")
+// cantidadSpan.innerHTML = cantidad
